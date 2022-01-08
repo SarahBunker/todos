a32706720679866968d24a254a1abd3cb21717c3
Sarah Bunker <chemencoder@gmail.com>
2022-01-08 11:25:02 -0700

    initial project files, added home page, add new list page, add imput validation.

---
+++ todo.rb
@@ -0,0 +1,56 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get "/" do
+  redirect "/lists"
+end
+
+# View list of lists
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+# Render the new list form
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+#return an error message if name is invalid. Return nill if name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    "The list name must be between 1 and 100 characters."
+  elsif session[:lists].any? {|list| list[:name] == name}
+    "The list name must be unique."
+  else
+    nil
+  end
+end
+
+# Create a new list
+post "/lists" do
+  list_name = params[:list_name].strip
+  if error = error_for_list_name(list_name)
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end
+end
+
+get "lists/1" do
+  "View single list"
+end
+
