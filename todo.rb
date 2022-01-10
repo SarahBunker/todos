2d9232beafa795de3932e9addb5e40d3a0d8f007
Sarah Bunker <chemencoder@gmail.com>
2022-01-10 15:21:32 -0700

    added edit list button, added delete list button, rubocop fixes

--- todo.rb
+++ todo.rb
@@ -46,7 +46,7 @@ post '/lists' do
     session[:error] = error
     erb :new_list, layout: :layout
   else
-    session[:lists] << { name: list_name, todos: ["lettuce", "meat"]}
+    session[:lists] << { name: list_name, todos: [] }
     session[:success] = 'The list has been created.'
     redirect '/lists'
   end
@@ -56,21 +56,46 @@ end
 get '/lists/:id' do
   id = params[:id].to_i
   @list = session[:lists][id]
-  erb :list_test, layout: :layout
-  # @number = id.to_i
-  # @todos = session[:lists][@number][:todos]
-  # erb :list, layout: :layout
+  erb :list, layout: :layout
 end
 
-get '/list/:id/edit' do
-  'Hello World'
+# Edit an existing todo list
+get '/lists/:id/edit' do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+
+  erb :list_edit, layout: :layout
+end
+
+# Update an existing todo list
+post '/lists/:id' do
+  list_name = params[:list_name].strip
+  id = params[:id].to_i
+  @list = session[:lists][id]
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :list_edit, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = 'The list has been updated.'
+    redirect "/lists/#{params[:id].to_i}"
+  end
+end
+
+post '/lists/:id/delete' do
+  id = params[:id].to_i
+  session[:lists].delete_at(id)
+  session[:success] = 'The list has been deleted.'
+  redirect '/lists'
 end
 
 post '/lists/:id' do |id|
   # todo_name = params[:todo_name]
 
   # @number = id.to_i
-  
+
   # lists = session[:lists]
   # list = lists[@number]
   # if list == nil
